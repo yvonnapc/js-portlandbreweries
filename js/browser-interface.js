5ef6e970957e388ea965e452ee22b5d17bf7b5f7
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-17 16:31:46 -0700

    Update browser-interface.js

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,12 +1,7 @@
-// var apiKey = require('./../.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 var initMap = require('./../js/map.js').initMap;
 
-//
 $(document).ready(function(){
   event.preventDefault();
   google.maps.event.addDomListener(window, 'load', initMap);
-  // var place = 
-  // $('#addToList').click(function(){
-  //   $('#results').append("hello" + place.name);
-  // });
 });
